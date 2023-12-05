55bdfd8f9208f49330ba8d409db84679fdf255e7
zhoum929 <89593559+zhoum929@users.noreply.github.com>
2023-12-04 21:12:07 -0500

    find RTP

---
+++ find_RTP.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string filename;
+ifstream fin;
+
+int main(){
+	string s;
+	cout<<"filename without extension: "<<endl;
+	cin>>s;
+	s+=".vipr";
+	fin.open(s);
+	while(true){
+		fin>>s;
+		if(s=="OBJ"){
+			fin>>s;
+			cout<<"problem type: "<<s<<endl;
+			break;
+		}
+	}
+	while(true){
+		fin>>s;
+		if(s=="RTP"){
+			fin>>s;
+			cout<<s<<endl;
+			if(s!="range")return 0;
+			fin>>s;
+			cout<<"lower bound: "<<s<<endl;
+			fin>>s;
+			cout<<"upper bound: "<<s<<endl;
+			break;
+		}
+	}
+	return 0;
+}
