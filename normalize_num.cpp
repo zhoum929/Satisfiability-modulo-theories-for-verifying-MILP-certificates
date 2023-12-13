ffa855a3faac3766f1c673ae00370c34fdb8d728
zhoum929 <89593559+zhoum929@users.noreply.github.com>
2023-12-13 07:58:35 -0500

    Update normalize_num.cpp

--- normalize_num.cpp
+++ normalize_num.cpp
@@ -11,37 +11,48 @@ string filename;
 ofstream fout;
 ifstream fin;
 
-int main(){
-	cout<<"filename without extension: ";
-	cin>>filename;
-	string s1,s2;
-	s1=filename+".smt2";
-	s2=filename+"_norm.smt2";
+int main()
+{
+	cout << "Please type the filename: " << endl;
+	cin >> filename;
+	int lnth = filename.length();
+	filename = filename.substr(0, lnth - 5);
+	cout << endl;
+	string s1, s2;
+	s1 = filename + ".smt2";
+	s2 = filename + "_norm.smt2";
 	fout.open(s2);
 	fin.open(s1);
-	
-	int lines=0;
-	//change minus sign into minus operator
-	while(true){
+
+	int lines = 0;
+	// change minus sign into minus operator
+	while (true)
+	{
 		string s;
-		fin>>s;
-		if(s.length()>1&&s[0]=='-'&&s[1]>='0'&&s[1]<='9'){
-			int lnth=s.length();
-			fout<<"(- ";
-			fout<<s.substr(1,lnth-1);
-			fout<<")";
-		}else fout<<s;
-		if(s.substr(0,6)=="(exit)"){
+		fin >> s;
+		if (s.length() > 1 && s[0] == '-' && s[1] >= '0' && s[1] <= '9')
+		{
+			int lnth = s.length();
+			fout << "(- ";
+			fout << s.substr(1, lnth - 1);
+			fout << ")";
+		}
+		else
+			fout << s;
+		if (s.substr(0, 6) == "(exit)")
+		{
 			fin.close();
 			fout.close();
 			exit(0);
 		}
 		char k;
-		k=fin.get();
-		//change line may not be same in different systems
-		if(k=='\n'||k=='r')fout<<endl;
-		else fout<<k;
+		k = fin.get();
+		// change line may not be same in different systems
+		if (k == '\n' || k == 'r')
+			fout << endl;
+		else
+			fout << k;
 	}
-	
+
 	return 0;
 }
