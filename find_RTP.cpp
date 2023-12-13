068feee3cbc4bbcab42030bcb92c9542b41177c3
zhoum929 <89593559+zhoum929@users.noreply.github.com>
2023-12-13 07:19:50 -0500

    Update find_RTP.cpp

--- find_RTP.cpp
+++ find_RTP.cpp
@@ -10,30 +10,35 @@ using namespace std;
 string filename;
 ifstream fin;
 
-int main(){
+int main()
+{
 	string s;
-	cout<<"filename without extension: "<<endl;
-	cin>>s;
-	s+=".vipr";
+	cout << "Please type the filename: " << endl;
+	cin >> s;
 	fin.open(s);
-	while(true){
-		fin>>s;
-		if(s=="OBJ"){
-			fin>>s;
-			cout<<"problem type: "<<s<<endl;
+	while (true)
+	{
+		fin >> s;
+		if (s == "OBJ")
+		{
+			fin >> s;
+			cout << "Minimization or Maximization problem: " << s << endl;
 			break;
 		}
 	}
-	while(true){
-		fin>>s;
-		if(s=="RTP"){
-			fin>>s;
-			cout<<s<<endl;
-			if(s!="range")return 0;
-			fin>>s;
-			cout<<"lower bound: "<<s<<endl;
-			fin>>s;
-			cout<<"upper bound: "<<s<<endl;
+	while (true)
+	{
+		fin >> s;
+		if (s == "RTP")
+		{
+			fin >> s;
+			cout << s << endl;
+			if (s != "range")
+				return 0;
+			fin >> s;
+			cout << "lower bound: " << s << endl;
+			fin >> s;
+			cout << "upper bound: " << s << endl;
 			break;
 		}
 	}
